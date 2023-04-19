c3d0df71bbcf0e2c7a0ac405dc767cb2da68b1c7
Lucas Del Puerto Garcia <lucas.d.puerto@outlook.com>
2023-04-18 22:37:27 -0300

    :zap: remove blank spaces

--- src/app.py
+++ src/app.py
@@ -7,23 +7,23 @@ from src.core.mqtt_server import send_data
 from update import start_update
 
 def init():
-	do_connect(envs['WIFI_SSID'], envs['WIFI_PASSWORD'])
-	start_update()
+    do_connect(envs['WIFI_SSID'], envs['WIFI_PASSWORD'])
+    start_update()
 
 def run():
-	api_key = envs['THINGSPEAK_WRITE_API_KEY']
-	time_interval = envs['UPDATE_TIME_INTERVAL']
+    api_key = envs['THINGSPEAK_WRITE_API_KEY']
+    time_interval = envs['UPDATE_TIME_INTERVAL']
 
-	last_update = ticks_ms()
+    last_update = ticks_ms()
+    
+    while True:
+        if ticks_ms() - last_update >= time_interval: 
+            try:
+                celsius, humidity = collect_data()
+                json_readings = { 'field1': celsius, 'field2': humidity } 
 
-	while True:
-		if ticks_ms() - last_update >= time_interval: 
-			try:
-				celsius, humidity = collect_data()
-				json_readings = { 'field1': celsius, 'field2': humidity } 
-				
-				send_data(json_readings, api_key)
-				
-				last_update = ticks_ms()
-			except:
-				print('Ocorreu um erro, tentando novamente')
+                send_data(json_readings, api_key)
+
+                last_update = ticks_ms()
+            except:
+                print('Ocorreu um erro, tentando novamente')
